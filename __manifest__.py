515f38acd50f5f4aa351a235819fdd5d2f193d61
Developer <m_mannaa2008@yahoo.com>
2020-04-08 23:24:10 +0300

    First Release

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Housemaid System 
     """,
 
-    'author': "My Company",
+    'author': "Medhat Mannaa",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
