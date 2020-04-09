85129a29e3114c5c2e3dbfc6a0501a0bc1f15b03
Developer <m_mannaa2008@yahoo.com>
2020-04-09 09:52:52 +0300

    First Release

--- models/reservation.py
+++ models/reservation.py
@@ -6,10 +6,10 @@ class reservation(models.Model):
     _description = 'Full Housemaid Reservation Transaction Information'
 
     application_id = fields.Many2one('housemaid.application', 'Housemaid')
+    reservation_date = fields.Date(string="Reservation Date", required=True, defualt=fields.Date.context_today, )
     customer_name = fields.Char(string="Customer Name", required=True, size=80, )
     customer_area = fields.Char(string="Customer Area", required=True, size=80, )
     customer_phone = fields.Char(string="Customer Phone", required=True, size=80, )
-    reservation_date = fields.Date(string="Reservation Date", required=True, defualt=fields.Date.context_today, )
     due_amount = fields.Integer(string="DUE Amount", required=True, copy=True)
     cash_payment_amount = fields.Integer(string="Cash Payment", default='0.00', required=True, copy=True)
     knet_payment_amount = fields.Integer(string="KNET Payment", default='0.00', required=True, copy=True)
