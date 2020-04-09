ba9743e10e01a66db7cc48df4aff2af6fd2b2a57
Developer <m_mannaa2008@yahoo.com>
2020-04-09 20:29:22 +0300

    First Release

--- models/reservation.py
+++ models/reservation.py
@@ -5,16 +5,16 @@ class reservation(models.Model):
     _name = 'housemaid.reservation'
     _description = 'Full Housemaid Reservation Transaction Information'
 
-    application_id = fields.One2many('housemaid.application', 'name')
+    application_id = fields.Many2one('housemaid.application', 'name', )
     reservation_date = fields.Date(string="Reservation Date", required=True, defualt=fields.Date.context_today, )
     customer_name = fields.Char(string="Customer Name", required=True, size=80, )
     customer_area = fields.Char(string="Customer Area", required=True, size=80, )
     customer_phone = fields.Char(string="Customer Phone", required=True, size=80, )
     due_amount = fields.Integer(string="DUE Amount", required=True, copy=True)
-    cash_payment_amount = fields.Integer(string="Cash Payment", default='0.00', required=True, copy=True)
-    knet_payment_amount = fields.Integer(string="KNET Payment", default='0.00', required=True, copy=True)
-    vise_payment_amount = fields.Integer(string="VISA Payment", default='0.00', required=True, copy=True)
-    other_payment_amount = fields.Integer(string="Other Payment", default='0.00', required=True, copy=True)
+    cash_payment_amount = fields.Integer(string="Cash Payment", default='0', required=True, copy=True)
+    knet_payment_amount = fields.Integer(string="KNET Payment", default='0', required=True, copy=True)
+    vise_payment_amount = fields.Integer(string="VISA Payment", default='0', required=True, copy=True)
+    other_payment_amount = fields.Integer(string="Other Payment", default='0', required=True, copy=True)
     other_payment_method = fields.Selection(selection=[('0', 'American Express'),
                                                        ('1', 'MasterCard'),
                                                        ('2', 'Western Union'),
@@ -25,3 +25,4 @@ class reservation(models.Model):
                                             string="Other Payment Method",
                                             required=False, )
     remarks = fields.Char(string="Remarks", required=False, size=255, )
+    reservation_status = fields.Integer(string="Reservation Status", default='0', required=True, copy=True)
--- models/visa.py
+++ models/visa.py
@@ -5,17 +5,17 @@ class visa(models.Model):
     _name = 'housemaid.visa'
     _description = 'Full Housemaid Visa Transaction Information'
 
-    application_id = fields.One2many('housemaid.application', 'name')
+    application_id = fields.Many2one('housemaid.application', 'name')
     customer = fields.Char(string="Customer", required=False, size=120, )
     visa_number = fields.Char(string="Visa Number", required=False, size=80, )
     labor_id = fields.Char(string="Labor ID", required=False, size=80, )
     visa_create_date = fields.Date(string="Visa Create Date", required=True, )
     visa_receive_date = fields.Date(string="Visa Receive Date", required=True, defualt=fields.Date.context_today, )
     visa_sent_date = fields.Date(string="Visa Sent Date", required=True, )
-    cash_payment_amount = fields.Integer(string="Cash Payment", default='0.00', required=True, copy=True)
-    knet_payment_amount = fields.Integer(string="KNET Payment", default='0.00', required=True, copy=True)
-    vise_payment_amount = fields.Integer(string="VISA Payment", default='0.00', required=True, copy=True)
-    other_payment_amount = fields.Integer(string="Other Payment", default='0.00', required=True, copy=True)
+    cash_payment_amount = fields.Integer(string="Cash Payment", default='0', required=True, copy=True)
+    knet_payment_amount = fields.Integer(string="KNET Payment", default='0', required=True, copy=True)
+    vise_payment_amount = fields.Integer(string="VISA Payment", default='0', required=True, copy=True)
+    other_payment_amount = fields.Integer(string="Other Payment", default='0', required=True, copy=True)
     other_payment_method = fields.Selection(selection=[('0', 'American Express'),
                                                        ('1', 'MasterCard'),
                                                        ('2', 'Western Union'),
@@ -26,3 +26,4 @@ class visa(models.Model):
                                             string="Other Payment Method",
                                             required=False, )
     remarks = fields.Char(string="Remarks", required=False, size=255, )
+    visa_status = fields.Integer(string="Visa Status", default='0', required=True, copy=True)
