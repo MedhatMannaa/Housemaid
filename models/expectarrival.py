302b9e32810ec001ef0b926554f54b637c970540
Developer <m_mannaa2008@yahoo.com>
2020-04-12 20:37:44 +0300

    First Release

--- models/expectarrival.py
+++ models/expectarrival.py
@@ -7,14 +7,15 @@ logger = logging.getLogger(__name__)
 
 class ExpectingArrival(models.Model):
     _name = 'housemaid.expectarrival'
-    _description = 'Full Housemaid Visa Transaction Information'
+    _description = 'Full Housemaid Expecting Arrival Transaction Information'
 
     name = fields.Many2one(comodel_name='housemaid.application', string='name', domain=[('state', '=', 'visa')])
     email_receive_date = fields.Date(string="Email Receive Date", required=False, defualt=fields.Date.context_today, )
     expecting_arrival_date = fields.Datetime(string="Expecting Arrival Date", required=True, )
     remarks = fields.Char(string="Remarks", required=False, size=255, )
-    state = fields.Selection(string="Visa Status", required=True,
+    state = fields.Selection(string="Expecting Arrival Status", required=True,
                               selection=[('active', 'Active Expecting Date'),
+                                         ('havevisa', 'Active Expecting Date With Visa'),
                                          ('canceled', 'Canceled Active Expecting'), ], default='active')
     # override create function
     @api.model
