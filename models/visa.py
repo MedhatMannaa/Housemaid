9109130bb68b100874d9124ae6b7734f4f4f6632
Developer <m_mannaa2008@yahoo.com>
2020-04-13 12:25:55 +0300

    First Release

--- models/visa.py
+++ models/visa.py
@@ -10,7 +10,7 @@ class Visa(models.Model):
     _description = 'Full Housemaid Visa Transaction Information'
 
     name = fields.Many2one(comodel_name='housemaid.application', string='name', domain=[('state', '=', 'reservation')])
-    visa_scan = fields.Image(string="Visa Image", )
+    visa_scan = fields.Image(string="Image", )
     customer = fields.Char(string="Customer", required=False, size=120, )
     visa_number = fields.Char(string="Visa Number", required=False, size=80, )
     labor_id = fields.Char(string="Labor ID", required=False, size=80, )
