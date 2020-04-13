a6277b2492cc278c4add0735ba3d1f0aa9cb5f83
Developer <m_mannaa2008@yahoo.com>
2020-04-13 09:09:55 +0300

    First Release

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import application
-from . import expectingarrival
 from . import externaloffices
 from . import reservation
 from . import visa
+from . import expectingarrival
 
